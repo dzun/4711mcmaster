52d8c387dfed4d3323a3ec71a13baf15dda9ef8f
Alistair Beck <allstarules@hotmail.com>
2011-10-24 19:21:23 -0700

    Updated CSS and League page

--- application/views/pages/league.php
+++ application/views/pages/league.php
@@ -13,13 +13,13 @@
             </select>
             <input type="submit" value="Submit"/>
         </form>
-        
+
         <div class="overflowTable">
             <?php
             $projects = new SimpleXMLElement
                             ('data/comp4711-cis-teams.xml', null, true);
 
-            
+
             if ($displayType == null || $displayType == '')
                 $displayType = "league";
 
@@ -27,7 +27,8 @@
                     <table border="1" class="table">
                         <tr>
                             <th>Institute</th>
-                            <th>Team</th>
+                            <th>Official Site</th>
+                            <th>Group Site</th>
                             <th>Member 1</th>
                             <th>Member 2</th>
                         </tr>
@@ -44,7 +45,12 @@ TABLE;
                                 {$group->institute}
                             </td>
                             <td>
-                                <a href="{$group->website}" target=_new>
+                                 <a href="{$group->website}" target=_new>
+                                    {$group->team} 
+                                </a>
+                            </td>
+                            <td>
+                                <a href="http://{$group['code']}.bcitxml.com" target=_new>
                                     {$group->team}
                                 </a>
                             </td>
@@ -62,13 +68,20 @@ LEAGUE;
                 echo '</table>';
             } elseif ($displayType == "conference")
             {
-                echo '<h3>Atlantic University</h3>';
-                echo $table;
-                foreach ($projects->teams->group as $group)
+                foreach ($projects->conferences as $conferences)
                 {
-                    if ($group->team['conf'] == "AUS")
+                    $confCount = $conferences->count();
+                }
+                for ($i = 0; $i < $confCount; $i++)
+                {
+                    echo '<h3>' . $conferences->conference[$i] . '</h3>';
+                    echo $table;
+                    foreach ($projects->teams->group as $group)
                     {
-                        echo <<<CONF
+                        if ($group->team['conf'] ==
+                                (string) $conferences->conference[$i]['code'])
+                        {
+                            echo <<<CONF
                         <tr></tr>
                         <tr>
                             <td>
@@ -76,6 +89,11 @@ LEAGUE;
                             </td>
                             <td>
                                 <a href="{$group->website}" target=_new>
+                                    {$group->team} 
+                                </a>
+                            </td>
+                            <td>
+                                <a href="http://{$group['code']}.bcitxml.com" target=_new>
                                     {$group->team}
                                 </a>
                             </td>
@@ -88,97 +106,12 @@ LEAGUE;
                                 {$group->members->member[1]}
                             </td>
                         </tr>
+                        
 CONF;
+                        }
                     }
+                    echo '</table>';
                 }
-                echo '</table>';
-                echo '<h3>Quebec University</h3>';
-                echo $table;
-                foreach ($projects->teams->group as $group)
-                {
-                    if ($group->team['conf'] == "RSEQ")
-                    {
-                        echo <<<CONF
-                        <tr>
-                            <td>
-                                {$group->institute}
-                            </td>
-                            <td>
-                                <a href="{$group->website}" target=_new>
-                                    {$group->team}
-                                </a>
-                            </td>
-                            <td>
-                                <a href="mailto:{$group->members->member['email']}">
-                                    {$group->members->member[0]}
-                                </a>
-                            </td>
-                            <td>
-                                {$group->members->member[1]}
-                            </td>
-                        </tr>
-CONF;
-                    }
-                }
-                echo '</table>';
-                echo '<h3>Ontario University</h3>';
-                echo $table;
-                foreach ($projects->teams->group as $group)
-                {
-                    if ($group->team['conf'] == "OUA")
-                    {
-                        echo <<<CONF
-                        <tr>
-                            <td>
-                                {$group->institute}
-                            </td>
-                            <td>
-                                <a href="{$group->website}" target=_new>
-                                    {$group->team}
-                                </a>
-                            </td>
-                            <td>
-                                <a href="mailto:{$group->members->member['email']}">
-                                    {$group->members->member[0]}
-                                </a>
-                            </td>
-                            <td>
-                                {$group->members->member[1]}
-                            </td>
-                        </tr>
-CONF;
-                    }
-                }
-                echo '</table>';
-                echo '<h3>Canada West</h3>';
-                echo $table;
-                foreach ($projects->teams->group as $group)
-                {
-                    if ($group->team['conf'] == "WEST")
-                    {
-                        echo <<<CONF
-                        <tr>
-                            <td>
-                                {$group->institute}
-                            </td>
-                            <td>
-                                <a href="{$group->website}" target=_new>
-                                    {$group->team}
-                                </a>
-                            </td>
-                            <td>
-                                <a href="mailto:{$group->members->member['email']}">
-                                    {$group->members->member[0]}
-                                </a>
-                            </td>
-                            <td>
-                                {$group->members->member[1]}
-                            </td>
-                        </tr>
-CONF;
-                    }
-                }
-                echo '</table>';
             }
             ?>
 
